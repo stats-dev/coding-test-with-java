57892f13acb78d190f691e43d2fac81432219875
stats-dev <hohun94@naver.com>
2023-03-13 15:24:16 +0900

    first java basic commit with type castings for java

---
+++ java-basic/src/java_type_castings.java
@@ -0,0 +1,17 @@
+
+public class java_type_castings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long longVal = 327701L;
+		int intVal = (int)longVal;
+		
+		System.out.println("이렇게 명시적인 형변환을 할 때는 괄호에 해당 타입을 입력해주면 됩니다.");
+		System.out.println(intVal);
+		System.out.println(longVal);
+		
+		
+
+	}
+
+}
