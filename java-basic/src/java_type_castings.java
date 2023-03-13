4e2d82ad8b053280375b2fcb9922ac29c8566783
stats-dev <hohun94@naver.com>
2023-03-13 15:36:17 +0900

    type castings for java updates

--- java-basic/src/java_type_castings.java
+++ java-basic/src/java_type_castings.java
@@ -2,16 +2,48 @@
 public class java_type_castings {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		long longVal = 327701L;
 		int intVal = (int)longVal;
 		
-		System.out.println("이렇게 명시적인 형변환을 할 때는 괄호에 해당 타입을 입력해주면 됩니다.");
+		System.out.println("명시적인 형변환: ");
 		System.out.println(intVal);
 		System.out.println(longVal);
 		
+//		명시적인 형변환: 
+//			327701
+//			327701
 		
+		System.out.println("묵시적인 형변환: ");
+		char chVal = 'A';
+		int intVal2 = chVal;
+		
+		double dPie = 3.1415;
+		int iNum = 13;
+		
+		System.out.println("묵시적인 형변환 chVal = " + chVal);
+		System.out.println("명시적인 형변환 (int)chVal = " + (int)chVal);
+		System.out.println("묵시적인 형변환 dPie+iNum = " + (dPie+iNum));
 
+//		묵시적인 형변환: 
+//			묵시적인 형변환 chVal = A
+//			명시적인 형변환 (int)chVal = 65
+//			묵시적인 형변환 dPie+iNum = 16.1415
+
+
+		System.out.println("문자열 결합시 형변환: ");
+		int dates = 20230313;
+		String str = "java";
+		System.out.println("dates = " + dates);
+		System.out.println("str = " + str);
+		System.out.println("str + dates=" + str + dates);
+		
+//		문자열 결합시 형변환: 
+//			dates = 20230313
+//			str = java
+//			str + dates=java20230313
+
+		
 	}
 
 }
